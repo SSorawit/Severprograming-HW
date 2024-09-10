f786da6211625d9de7d4298b9d4d81fc0d3a6064
LoRaAgent <sowarit007@gmail.com>
2024-09-10 22:15:19 +0700

    up

--- HW5/calculator.js
+++ HW5/calculator.js
@@ -34,4 +34,100 @@ function cal() {
     result = result / 1000000;
     result = result + "M";
   }
+  
+  
+  document.getElementById("output-this").innerHTML = ` ${result} \tOhms  \t${selectTolerance} \t ${selectPPm}`;
+  
+  document.getElementById("band-number1").style.backgroundColor = color[parseInt(selectValue_st1 , 10)];
+  document.getElementById("band-number2").style.backgroundColor = color[parseInt(selectValue_st2 , 10)];
+  document.getElementById("band-number3").style.backgroundColor = color[parseInt(selectValue_st3 , 10)];
+  document.getElementById("band-multiplier").style.backgroundColor = multicor[multipliercal(seletMultiplier)];
+  document.getElementById("band-tolerance").style.backgroundColor = tolerancecor[tolerancecal(selectTolerance)];
+  document.getElementById("band-ppm").style.backgroundColor = ppmcor[ppmcal(selectPPm)];
+  
+} 
+
+function tolerancecal(tolerance) {
+  if (tolerance === "± 10%") {
+    return 0;
+  }
+  else if (tolerance === "± 5%") {
+    return 1;
+  }
+  else if (tolerance === "± 1%") {
+    return 2;
+  }
+  else if (tolerance === "± 2%") {
+    return 3;
+  }
+  else if (tolerance === "± 0.5%") {
+    return 4;
+  }
+  else if (tolerance === "± 0.25%") {
+    return 5;
+  }
+  else if (tolerance === "± 0.1%") {
+    return 6;
+  }
+  
+}
+
+function multipliercal(sum) {
+  switch (sum) {
+    case 0.01:
+      sum = 0;
+      return sum;
+      break;
+    case 0.1:
+      sum = 1;
+      return sum;
+      break;
+    case 1:
+      sum = 2;
+      return sum;
+      break;
+    case 10:
+      sum = 3;
+      return sum;
+      break;
+    case 100:
+      sum = 4;
+      return sum;
+      break;
+    case 1000:
+      sum = 5;
+      return sum;
+      break;
+    case 10000:
+      sum = 6;
+      return sum;
+      break;
+    case 100000:
+      sum = 7;
+      return sum;
+      break;
+    case 1000000:
+      sum = 8;
+      return sum;
+      break;
+    case 10000000:
+      sum = 9;
+      return sum;
+      break;
+  }
+}
+
+function ppmcal(ppm) {
+  if (ppm === "100") {
+    return 0;
+  } 
+  else if (ppm === "50") {
+    return 1;
+  }
+  else if (ppm === "15") {
+    return 2;
+  }
+  else if (ppm === "25") {
+    return 3;
+  }
 }
